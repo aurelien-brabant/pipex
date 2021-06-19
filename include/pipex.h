214b77ecb462a1039a4c7a283cfa4340ddfc70c6
abrabant <abrabant@student.42.fr>
2021-06-19 14:51:03 +0200

    Add library function's prototypes

--- include/pipex.h
+++ include/pipex.h
@@ -1,3 +1,11 @@
 #ifndef PIPEX_H
 # define PIPEX_H
+# include <stddef.h>
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+char	*ft_strtok(char *str, const char *delim);
+int		ft_strncmp(const char *s1, const char *s2, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
 #endif
--- src/paths.c
+++ src/paths.c
@@ -1,7 +1,21 @@
 #include <stdlib.h>
+#include <stdio.h>
+
+#include "pipex.h"
 
 char	**get_paths(char **env)
 {
-	(void)env;
+	char	*path_value;
+
+	path_value = NULL;
+	while (*env)
+	{
+		if (ft_strncmp(*env, "PATH", 4) == 0 && *(*env + 4) == '=')
+			path_value = ft_strdup(*env + 5);
+		++env;
+	}
+	if (path_value == NULL)
+		printf("PATH is not set!");
+	free(path_value);
 	return (NULL);
 }
